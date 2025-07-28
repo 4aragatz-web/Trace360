07202c99d5c36ab03e64d6e34c66daa2d900e0bb
Adam Ragatz <4aragatz@gmail.com>
2025-07-28 15:29:50 +1000

    Fix typo in API URL

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   // Load products from backend on mount
   useEffect(() => {
-    axios.get('http://trace360-co.onrender.com/products')
+    axios.get('https://trace360-co.onrender.com/products')
       .then(res => setProducts(res.data))
       .catch(err => console.error(err));
   }, []);
@@ -81,8 +81,8 @@ function App() {
 
   // Save (add or update) a product to backend and update state
   const handleSaveNewProduct = (newProduct) => {
-    axios.post('http://trace360-co.onrender.com/products', newProduct)
-      .then(() => axios.get('http://trace360-co.onrender.com/products'))
+    axios.post('https://trace360-co.onrender.com/products', newProduct)
+      .then(() => axios.get('https://trace360-co.onrender.com/products'))
       .then(res => {
         setProducts(res.data);
         setShowDetails(true);
